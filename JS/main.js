020536f6f60fabaa114def235be7688254ccf833
Cooper Brown <cooperbrown.dev@gmail.com>
2025-06-29 20:52:34 -0500

    i More codewars

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,43 @@
+// Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+
+function opposite(number) {
+  //your code here
+}
+
+// My solution
+function opposite(number) {
+  return -number
+}
+
+// ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+// Examples
+// makeNegative(1);    // return -1
+// makeNegative(-5);   // return -5
+// makeNegative(0);    // return 0
+// makeNegative(0.12); // return -0.12
+// Notes
+// The number can be negative already, in which case no change is required.
+// Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
+function makeNegative(num) {
+  // Code?
+}
+
+// My Solution
+function makeNegative(num) {
+  return -Math.abs(num)
+}
+
+// ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 // Complete the solution so that it reverses the string passed into it.
 
 // 'world'  =>  'dlrow'
