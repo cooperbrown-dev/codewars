9ad2ea9f9697122e6d02d553e652f903484f6dfd
Cooper Brown <cooperbrown.dev@gmail.com>
2025-07-01 18:47:49 -0500

    7/1/25 Codewars Challenge

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,36 @@
+// Task
+// You get an array of numbers, return the sum of all of the positives ones.
+
+// Example
+// [1, -4, 7, 12] => 
+// 1
+// +
+// 7
+// +
+// 12
+// =
+// 20
+// 1+7+12=20
+// Note
+// If there is nothing to sum, the sum is default to 0.
+function positiveSum(arr) {
+  
+}
+
+// My Solution
+function positiveSum(arr) {
+  let sum = 0;
+  arr.forEach(num => {
+    if(num > 0){
+    sum += num;
+  }
+});
+  return sum;
+}
+
+
+// ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 // Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
 
 function boolToWord( bool ){
