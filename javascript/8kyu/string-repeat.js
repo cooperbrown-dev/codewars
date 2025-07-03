3463a1ca2de520d3d21b90ee0c1e53820485a6bf
Cooper Brown <cooperbrown.dev@gmail.com>
2025-07-02 22:24:49 -0500

    Codewars 7/2/25

---
+++ javascript/8kyu/string-repeat.js
@@ -0,0 +1,19 @@
+// Write a function that accepts a non-negative integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+// Examples (input -> output)
+// 6, "I"     -> "IIIIII"
+// 5, "Hello" -> "HelloHelloHelloHelloHello"
+// FundamentalsStrings
+
+// function removeChar(str){
+//  //You got this!
+
+// };
+
+
+// My Solution:
+
+function removeChar(str){
+ return str.slice(1, str.length - 1)
+ 
+};
