93a82add59ae0c2a04a66e3307ac1b6565a797e7
Cooper Brown <cooperbrown.dev@gmail.com>
2025-07-27 00:01:00 -0500

    Code wasn't correct, had to make some changes

--- javascript/8kyu/2025-07-26_count-of-positives-sum-of-negatives.js
+++ javascript/8kyu/2025-07-26_count-of-positives-sum-of-negatives.js
@@ -7,19 +7,25 @@
 // Example
 // For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
 
+// My solution:
 function countPositivesSumNegatives(input) {
+  if (!input || input.length === 0){
+      return [];
+  }
   let posSum = 0;
   let negSum = 0;
-  let emptyArr = [];
   for (let i = 0; i < input.length; i++){
-    if (input[i] > 0){
+    if(input[i] > 0){
       posSum += 1;
     }else if(input[i] < 0){
-      negSum += 1;
-    }else{
-      return emptyArr
+      negSum += input[i]
     }
   }
   let posNegArr = [posSum,negSum];
   return posNegArr
+  }
+
+// Other solution for review:
+  function countPositivesSumNegatives(input) {
+    return input && input.length ? [input.filter(p => p > 0).length, input.filter(n => n < 0).reduce((a, b) => a + b, 0)] : [];
 }
