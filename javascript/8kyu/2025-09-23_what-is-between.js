0f0da7cef7196f8221c22e84b38345d282ee57ef
Cooper Brown <cooperbrown.dev@gmail.com>
2025-09-23 17:55:07 -0500

    Codewars 9/23/25

---
+++ javascript/8kyu/2025-09-23_what-is-between.js
@@ -0,0 +1,15 @@
+// function between(a, b) {
+//   let newArray = [];
+//   for (let i = a; a <= b; i++){
+//     newArray.push(i);
+//   }
+//   return newArray;
+// }
+
+function between(a, b) {
+  let newArray = [];
+  for (let i = a; i <= b; i++){
+    newArray.push(i);
+  }
+  return newArray;
+}
